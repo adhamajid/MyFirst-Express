e5b88020685be7dfb6931bc4c2277add81a1a5cb
adhamajid <adha1900018252@webmail.uad.ac.id>
2024-06-29 16:45:58 +0700

    app.json

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
+  res.json({
+    nama :'Adha Syah Majid',
+    email : 'adhasyahm@gmail.com',
+    noHp : '08994163562',
+  })
 });
+
+app.get("/about", (req, res) => {
+  res.send("About Me!");
+})
 
 //Taruh ini paling bawah setalh app.get untuk route/halaman kosong
 app.use("/", (req, res) => {
